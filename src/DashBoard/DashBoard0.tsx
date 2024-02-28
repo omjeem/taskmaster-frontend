5fcb8e39a2e509dfc05aca39fd47763ad0d2724d
Om Jee Mishra <93421142+om-jee-m@users.noreply.github.com>
2024-02-28 17:33:09 +0530

    initial page message

--- src/DashBoard/DashBoard0.tsx
+++ src/DashBoard/DashBoard0.tsx
@@ -7,29 +7,29 @@ const API = import.meta.env.VITE_API_URL;
 
 export function DashBoard0() {
     const navigate = useNavigate()
-    useEffect(() => {
-        const token = localStorage.getItem("token")
-        if (!token) {
-            navigate("/main")
-        }
-        else {
-            axios.get(API + "api/user/", {
-                headers: {
-                    "Authorization": token,
-                    "Content-Type": "application/json"
-                }
-            }).then(() => {
-                navigate("/dashboard")
-            }).catch(() => {
-                navigate("/main")
+    // useEffect(() => {
+    //     const token = localStorage.getItem("token")
+    //     if (!token) {
+    //         navigate("/main")
+    //     }
+    //     else {
+    //         axios.get(API + "api/user/", {
+    //             headers: {
+    //                 "Authorization": token,
+    //                 "Content-Type": "application/json"
+    //             }
+    //         }).then(() => {
+    //             navigate("/dashboard")
+    //         }).catch(() => {
+    //             navigate("/main")
 
-            })
-        }
+    //         })
+    //     }
 
-    }, [navigate])
+    // }, [navigate])
     return <div className="flex justify-center items-center h-full pb-40">
         <div className="text-center items-center pt-48">
-            <h2 className="text-3xl font-bold mb-2">Please be patient while we fetch your details. Our backend is hosted on a <br></br>serverless cloud provider, which may take longer than usual.</h2>
+            <h2 className="text-3xl font-bold mb-2">Thank you for your patience as we retrieve your details.<br></br> Our backend operates on a serverless cloud provider,<br></br> which may require additional time for the initial server activation.</h2>
         </div>
     </div>
 
